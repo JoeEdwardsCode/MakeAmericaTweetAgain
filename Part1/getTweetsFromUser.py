b6db81c804f26c17cc25b195f61a1d46662b75f1
JoeDevelops <joe@joedevelops.com>
2016-07-30 10:51:49 -0700

    Error Correction:  Updated line 22 to keep track of tweetIndex

--- Part1/getTweetsFromUser.py
+++ Part1/getTweetsFromUser.py
@@ -19,6 +19,7 @@ def main():
             tweets = api.user_timeline(screen_name=arg, include_retweets=False, max_id=tweetIndex)
             for tweet in tweets:
                 tweetResults.append(tweet.text)
+                tweetIndex = tweet.id
             time.sleep(REQUEST_DELAY)
 
     except IndexError:
