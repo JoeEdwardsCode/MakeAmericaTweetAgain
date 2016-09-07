e10ca81a75ea114ab6db03ec4dec022ee1175692
JoeDevelops <joe@joedevelops.com>
2016-09-06 19:37:21 -0700

    final commit Part3

--- Part3/tweetScheduler.py
+++ Part3/tweetScheduler.py
@@ -2,7 +2,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from os import system
 from time import sleep
 from datetime import datetime
-import json
 
 from api import getAPI
 from tweetGenerator import TrumpTweetGenerator
@@ -11,19 +10,19 @@ print("tweetScheduler.py Loaded @ {}".format(datetime.now()))
 
 # Edit Global TWITTER_NAME for Different User
 TWITTER_NAME = "realDonaldTrump"
-TWEET_WINDOW = True
 FOLLOWERS = []
 
 try:
     print("Attempting to Load/Get New Tweet Set")
     try:
+        # Load Existing Tweet Set if We Already Have One
         GENERATOR = TrumpTweetGenerator(TWITTER_NAME + "Tweets")
         print("Tweet Generator Created @ {}".format(datetime.now()))
     except:
+        # Gather New Tweet Set
         print("Getting Most Current Tweets @ {}".format(datetime.now()))
         system("python getTweetsFromUser.py " + TWITTER_NAME)
         print("Tweet Set Collected @ {}".format(datetime.now()))
-        # If not using TrumpTweetGenerator object, change to TweetGenerator object
         GENERATOR = TrumpTweetGenerator(TWITTER_NAME + "Tweets")
         print("Tweet Generator Created @ {}".format(datetime.now()))
     API = getAPI()
@@ -44,46 +43,52 @@ def updateTweets():
         print("Tweet Set Update Failed @ {}".format(datetime.now()))
 
 def postTweet():
-    if TWEET_WINDOW:
-        tweet = GENERATOR.generateTrumpTweet()
+    date = datetime.now()
+    if date.hour >= 8 and date.hour <= 20:
+        tweet = cleanTweet(GENERATOR.generateTrumpTweet())
         try:
             if rateLimitNotExceeded():
                 API.update_status(tweet)
                 print("Tweet Posted @ {}".format(datetime.now()))
                 print("--- {}".format(tweet))
-            else:
-                print("API Calls Maxed Out @ {}".format(datetime.now()))
         except:
             print("Tweet Posting Failed @ {}".format(datetime.now()))
     else:
         print("Tweet Posting Delayed. Resumes at 8:00 AM")
 
-def likeFollowers():
+def friendFollowers():
     print("Getting Followers List @ {}".format(datetime.now()))
     if rateLimitNotExceeded():
         followers = API.followers()
         if followers:
             for follower in followers:
-                if follower.id not in FOLLOWERS
+                if follower.id not in FOLLOWERS:
                     if rateLimitNotExceeded():
                         print("Creating Friendship with User: {}".format(follower.id))
                         API.create_friendship(follower.id)
                         FOLLOWERS.append(follower.id)
-                    else:
-                        print("API Calls Maxed Out @ {}".format(datetime.now()))
 
-def likeTweets():
+def likeAndReplyToMAGA():
     print("Liking Tweets @ {}".format(datetime.now()))
     if rateLimitNotExceeded():
-        tweets = API.search("@"+TWITTER_NAME)
+        tweets = API.search("MakeAmericaGreatAgain")
         for tweet in tweets:
             if rateLimitNotExceeded():
                 API.create_favorite(tweet.id)
-            else:
-                print("API Calls Maxed Out @ {}".format(datetime.now()))
-    else:
-        print("API Calls Maxed Out @ {}".format(datetime.now()))
+            if rateLimitNotExceeded():
+                reply = cleanTweet(GENERATOR.generateTrumpTweet())
+                API.update_status(reply, tweet.id)
 
+def likeAndFollowRetweets():
+    print("Liking Retweets & following poster @ {}".format(datetime.now()))
+    if rateLimitNotExceeded():
+        retweets = API.retweets_of_me()
+        for tweet in retweets:
+            if rateLimitNotExceeded():
+                API.create_favorite(tweet.id)
+            if rateLimitNotExceeded() and not tweet.user.id in FOLLOWERS:
+                API.create_friendship(tweet.user.id)
+                FOLLOWERS.append(tweet.user.id)
 
 def rateLimitNotExceeded():
     status = API.rate_limit_status()['resources']['application']['/application/rate_limit_status']['remaining']
@@ -93,20 +98,19 @@ def rateLimitNotExceeded():
         print("Rate Limit Status: Rate Limit Exceeded @ {}".format(datetime.now()))
     return status > 0
 
-def closeTweetWindow():
-    TWEET_WINDOW = False
-
-def openTweetWindow():
-    TWEET_WINDOW = True
-
+def cleanTweet(tweet):
+    if tweet.count('"') == 1:
+        tweet.replace('"', '')
+    if "amp;" in tweet:
+        tweet.replace("amp;", "")
+    return tweet
 
 tweetScheduler = BackgroundScheduler()
-tweetScheduler.add_job(closeTweetWindow, 'cron', hour=21, id='closeTweetWindow')
-tweetScheduler.add_job(openTweetWindow, 'cron', hour=8, id='openTweetWindow')
 tweetScheduler.add_job(postTweet, 'interval', minutes=15, id='postTweet')
 tweetScheduler.add_job(updateTweets, 'cron', hour=23, minute=59, id='updateTweets')
-tweetScheduler.add_job(likeFollowers, 'cron', hour=22, id='likeFollowers')
-tweetScheduler.add_job(likeTweets, 'cron', hour=7, id='likeTweets')
+tweetScheduler.add_job(friendFollowers, 'cron', hour=22, id='friendFollowers')
+tweetScheduler.add_job(likeAndReplyToMAGA, 'cron', hour=7, id='likeAndReplyToMAGA')
+tweetScheduler.add_job(likeAndFollowRetweets, 'cron', hour=5, id='likeAndFollowRetweets')
 tweetScheduler.start()
 while True:
     sleep(60)
